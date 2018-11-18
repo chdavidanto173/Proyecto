b2fc846a787222ef15992a5f360fc37f9c357851
chdavidanto173 <davidchacon93@hotmail.com>
2018-11-18 10:36:43 -0600

    Proyecto 4
    Proyecto 4

---
+++ Proyecto4/Proyecto4/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        private bool prueba()
+        {
+            // Método que inserta en BBDD.
+
+            return true;
+
+        }
+
+
+        [HttpPost]
+        public JsonResult crearBD()
+        {
+            if (prueba())
+                return Json("'Success':'true'");
+            else
+                return Json(String.Format("'Success':'false','Error':'Ha habido un error al insertar el registro.'"));
+        }
+
+    }
+}
---
+++ Proyecto4/Proyecto4/Scripts/Principal.js
@@ -0,0 +1,27 @@
+﻿
+$(document).ready(function () {
+
+    $("#ac").click(function () {
+  
+
+        $.ajax({
+            type: "POST",
+            url: '@Url.Action("crearBD","Home")',
+            content: "application/json; charset=utf-8",
+            dataType: "json",
+            data: null,
+            success: function (d) {
+                if (d.success == true)
+                    alert('Se ha creado la base de datos!!');
+                else { }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                alert('Error!!');
+            }
+        });
+    });
+   
+
+});
+
+
